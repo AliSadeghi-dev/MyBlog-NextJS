e22038594fc5b383a89b55e22847f60b7be2abaf
Alisadeghi <alisadghi228@gmail.com>
2023-01-10 18:48:20 +0330

    add project to github

---
+++ pages/contact.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+import ContactForm from "../components/contact/contact-form";
+
+function ContactPage() {
+    return ( <
+        Fragment >
+        <
+        Head >
+        <
+        title > Contact Me | Ali Taggart < /title> <
+        meta name = "description"
+        content = "Send me your messages." / >
+        <
+        /Head> <
+        ContactForm / >
+        <
+        /Fragment>
+    );
+}
+
+export default ContactPage;
